8b1223a1643481cc86fd566ba36dcd1b061be5e2
dhaneshvl <dhaneshwaranvl@gmail.com>
2024-08-15 21:33:10 +0530

    first commit

---
+++ src/main/java/com/turf/tracker/TurfTrackerApplication.java
@@ -0,0 +1,22 @@
+package com.turf.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TurfTrackerApplication {
+
+    @GetMapping("/welcome/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    public static void main(String[] args) {
+		SpringApplication.run(TurfTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turf/tracker/TurfTrackerApplicationTest.java
@@ -0,0 +1,28 @@
+package com.turf.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcResultHandlersDsl;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest
+class TurfTrackerApplicationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testWelcomeEndpoint() throws Exception {
+
+        String name = "Dhanesh";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/welcome/{name}", name))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello " + name));
+
+    }
+
+}
